089839f0ff89fd4c288bbfbe36c39ee53c8a2cfe
MrsIsa <78546346+MrsIsaa@users.noreply.github.com>
2025-04-09 00:01:53 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,4 +3,5 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const getLatestTweets = require('./functions');
+console.log(process.env.ACCOUNT_ID, process.env.BEARER_TOKEN, process.env.WEBHOOK_URL)
 getLatestTweets(process.env.ACCOUNT_ID);
